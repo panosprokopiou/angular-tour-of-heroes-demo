616396317e5c06c059c69ba7e751dd5a7bca31cb
Panagiotis Angelidakis <panagiotisa@lsnova.gr>
2021-06-04 17:34:56 +0300

    Create Project / Done!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,5 +5,5 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  title = 'angular-tour-of-heroes-prokop';
+  title = 'Tour of Heroes';
 }
