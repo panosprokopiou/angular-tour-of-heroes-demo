96e886bded65837e120f654b859e94a1a1998bf4
Panagiotis Angelidakis <panagiotisa@lsnova.gr>
2021-06-04 18:15:36 +0300

    Section 1 / Show hero details

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../domain/hero.interface';
 
 @Component({
   selector: 'app-heroes',
@@ -7,7 +8,10 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  hero = 'Windstorm';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
 
   constructor() {
   }
